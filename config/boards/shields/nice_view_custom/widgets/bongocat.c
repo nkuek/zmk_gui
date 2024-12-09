8697ba0b226447e2ec374a314da4695758441d18
nkuek <nkuek1@gmail.com>
2024-12-09 17:28:23 -0500

    revert back to position

--- config/boards/shields/nice_view_custom/widgets/bongocat.c
+++ config/boards/shields/nice_view_custom/widgets/bongocat.c
@@ -1,23 +1,22 @@
 #include <zephyr/kernel.h>
 #include <lvgl.h>
-#include <zmk/display.h>
 #include <zmk/event_manager.h>
-#include <zmk/events/wpm_state_changed.h>
-#include <zmk/wpm.h>
+#include <zmk/events/position_state_changed.h>
 #include "bongocat.h"
 #include "util.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-struct wpm_state {
-    uint8_t wpm;
-};
-
-static void set_animation_state(struct zmk_widget_bongocat *widget, bool use_left) {
+static void set_animation_state(struct zmk_widget_bongocat *widget, bool left_pressed, bool right_pressed) {
     const lv_img_dsc_t* frame;
     
-    if (widget->is_typing) {
-        frame = use_left ? &bongocat_left : &bongocat_right;
+    // Determine which frame to show
+    if (left_pressed && right_pressed) {
+        frame = &bongocat_both;
+    } else if (left_pressed) {
+        frame = &bongocat_left;
+    } else if (right_pressed) {
+        frame = &bongocat_right;
     } else {
         frame = &bongocat_default;
     }
@@ -29,39 +28,37 @@ static void set_animation_state(struct zmk_widget_bongocat *widget, bool use_lef
     }
 }
 
-static void handle_wpm_state_changed(struct zmk_widget_bongocat *widget, struct wpm_state state) {
-    bool was_typing = widget->is_typing;
-    widget->is_typing = (state.wpm > 0);
+static void handle_position_state_changed(struct zmk_widget_bongocat *widget, const struct zmk_position_state_changed *ev) {
+    bool is_right = (ev->position % 12) >= 15;
     
-    // If we just started typing or are still typing, alternate frames
-    if (widget->is_typing) {
-        widget->use_left = !widget->use_left;
+    if (is_right) {
+        widget->right_pressed = ev->state;
+    } else {
+        widget->left_pressed = ev->state;
     }
     
-    set_animation_state(widget, widget->use_left);
+    set_animation_state(widget, widget->left_pressed, widget->right_pressed);
 }
 
-static void wpm_state_changed_cb(struct wpm_state state) {
+static void position_state_changed_cb(zmk_event_t *eh) {
     struct zmk_widget_bongocat *widget;
+    const struct zmk_position_state_changed *ev = as_zmk_position_state_changed(eh);
+    if (!ev) return;
     
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
-        handle_wpm_state_changed(widget, state);
+        handle_position_state_changed(widget, ev);
     }
 }
 
-static struct wpm_state wpm_state_get_state(const zmk_event_t *eh) {
-    return (struct wpm_state){.wpm = zmk_wpm_get_state()};
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_bongocat, struct wpm_state, wpm_state_changed_cb, wpm_state_get_state);
-ZMK_SUBSCRIPTION(widget_bongocat, zmk_wpm_state_changed);
+ZMK_LISTENER(widget_bongocat, position_state_changed_cb);
+ZMK_SUBSCRIPTION(widget_bongocat, zmk_position_state_changed);
 
 int zmk_widget_bongocat_init(struct zmk_widget_bongocat *widget, lv_obj_t *parent) {
     widget->obj = lv_img_create(parent);
     lv_img_set_src(widget->obj, &bongocat_default);
     
-    widget->is_typing = false;
-    widget->use_left = false;
+    widget->left_pressed = false;
+    widget->right_pressed = false;
     widget->current_frame = &bongocat_default;
     
     sys_slist_append(&widgets, &widget->node);
