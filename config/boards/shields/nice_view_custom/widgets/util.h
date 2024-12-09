ea8116ad2844772ec167438b420dba58a3f2564e
nkuek <nkuek1@gmail.com>
2024-12-09 18:59:49 -0500

    try new approach

--- config/boards/shields/nice_view_custom/widgets/util.h
+++ config/boards/shields/nice_view_custom/widgets/util.h
@@ -5,30 +5,34 @@
  *
  */
 
+#pragma once
+
 #include <lvgl.h>
 #include <zmk/endpoints.h>
 
 #define CANVAS_SIZE 68
 
-#define LVGL_BACKGROUND                                                                            \
-    IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
-#define LVGL_FOREGROUND                                                                            \
-    IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_white() : lv_color_black()
+#define LVGL_BACKGROUND IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
+#define LVGL_FOREGROUND IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_white() : lv_color_black()
+
+extern const lv_img_dsc_t bongocat_default;
+extern const lv_img_dsc_t bongocat_left;
+extern const lv_img_dsc_t bongocat_right; 
+extern const lv_img_dsc_t bongocat_both;
 
 struct status_state {
     uint8_t battery;
     bool charging;
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     struct zmk_endpoint_instance selected_endpoint;
     int active_profile_index;
     bool active_profile_connected;
     bool active_profile_bonded;
     uint8_t layer_index;
     const char *layer_label;
-    uint8_t wpm[10];
-#else
-    bool connected;
-#endif
+    // Bongocat state
+    bool left_pressed;
+    bool right_pressed;
+    const lv_img_dsc_t* current_frame;
 };
 
 struct battery_status_state {
@@ -40,8 +44,7 @@ struct battery_status_state {
 
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]);
 void draw_battery(lv_obj_t *canvas, const struct status_state *state);
-void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
-                    lv_text_align_t align);
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font, lv_text_align_t align);
 void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color);
 void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width);
 void init_arc_dsc(lv_draw_arc_dsc_t *arc_dsc, lv_color_t color, uint8_t width);
