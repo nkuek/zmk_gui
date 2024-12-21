3cf562f39664365fc7890b0688519a8848780c45
nkuek <nkuek1@gmail.com>
2024-12-20 23:43:38 -0500

    crying

--- config/boards/shields/nice_view_custom/behaviors/art_cycle.c
+++ config/boards/shields/nice_view_custom/behaviors/art_cycle.c
@@ -4,7 +4,7 @@
 #include <zephyr/logging/log.h>
 #include <zmk/behavior.h>
 #include <zmk/split/bluetooth/peripheral.h>
-#include <drivers/behavior/behavior_driver.h>
+#include <drivers/behavior.h>
 
 #include "art_cycle.h"
 
